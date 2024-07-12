2850b87be4213199fd05130b8bb9e98ad26f0bba
rikiriki36 <rikirine_oiko@yahoo.co.jp>
2024-07-12 20:19:50 +0900

    java第4章課題

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,35 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		int testScoreA = 10;
+		int testScoreB = 20;
+		int testScoreC = 30;
+		int testScoreD = 40;
+		int testScoreE = 50;
+		int testScoreF = 60;
+		int testScoreG = 70;
+		int testScoreH = 80;
+		int testScoreI = 90;
+		int testScoreJ = 100;
+		int testAverage;
+
+		testAverage = ((testScoreA + testScoreB + testScoreC + testScoreD + testScoreE + testScoreF + testScoreG + testScoreH + testScoreI + testScoreJ)/10);
+		
+		System.out.println("Aさんの点数：" + testScoreA + "点");
+		System.out.println("Bさんの点数：" + testScoreB + "点");
+		System.out.println("Cさんの点数：" + testScoreC + "点");
+		System.out.println("Dさんの点数：" + testScoreD + "点");
+		System.out.println("Eさんの点数：" + testScoreE + "点");
+		System.out.println("Fさんの点数：" + testScoreF + "点");
+		System.out.println("Gさんの点数：" + testScoreG + "点");
+		System.out.println("Hさんの点数：" + testScoreH + "点");
+		System.out.println("Iさんの点数：" + testScoreI + "点");
+		System.out.println("Jさんの点数：" + testScoreJ + "点");
+
+		System.out.println("10人の平均点数：" + testAverage + "点");
+
+	}
+
+}
