3ff01098d1009775d71f52925716098b647b2854
rikiriki36 <rikirine_oiko@yahoo.co.jp>
2024-07-15 18:14:48 +0900

    java第14章課題

---
+++ kadai_014/Car_Chapter14.java
@@ -0,0 +1,11 @@
+package kadai_014;
+
+public class Car_Chapter14 {
+	public static void main(String[] args) {
+		
+		CarExec_Chapter14 car = new CarExec_Chapter14(1, 10);
+		
+		car.gearChange(3);
+		car.run();
+	}
+}
