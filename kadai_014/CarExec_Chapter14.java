0ada2ee7dfdb95ca393bddd1a51df652028ce174
rikiriki36 <rikirine_oiko@yahoo.co.jp>
2024-07-16 19:16:59 +0900

    java第14章課題-2

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -1,27 +1,12 @@
 package kadai_014;
 
 public class CarExec_Chapter14 {
-	private int gear = 1;
-	private int speed = 10;
 
-//コンストラクタ
-	public CarExec_Chapter14(int gear, int spped) {
-		this.gear = gear;
-		this.speed = speed;
-	}
-
-//	速度変更
-	public void gearChange(final int afterGear) {
-		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
-
-		switch(afterGear) {
-			case 1,2,3,4,5 ->  this.speed = afterGear * 10;
-			default -> this.speed = 10;
-		}
-	}
-
-//	ギアチェンジ後の速度を表示
-	public void run() {
-		System.out.println("速度は時速" + this.speed + "kmです");
+	public static void main(String[] args) {
+		
+		Car_Chapter14 car = new Car_Chapter14(1, 10);
+		
+		car.gearChange(3);
+		car.run();
 	}
 }
--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -1,11 +1,27 @@
 package kadai_014;
 
 public class Car_Chapter14 {
-	public static void main(String[] args) {
-		
-		CarExec_Chapter14 car = new CarExec_Chapter14(1, 10);
-		
-		car.gearChange(3);
-		car.run();
+	private int gear = 1;
+	private int speed = 10;
+
+//コンストラクタ
+	public Car_Chapter14(int gear, int spped) {
+		this.gear = gear;
+		this.speed = speed;
+	}
+
+//	速度変更
+	public void gearChange(final int afterGear) {
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+
+		switch(afterGear) {
+			case 1,2,3,4,5 ->  this.speed = afterGear * 10;
+			default -> this.speed = 10;
+		}
+	}
+
+//	ギアチェンジ後の速度を表示
+	public void run() {
+		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 }
